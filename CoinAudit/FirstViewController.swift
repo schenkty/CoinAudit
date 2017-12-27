4b356cc1d0f00de4b9b971915105357995ee074f
Ty Schenk <7807169+schenkty@users.noreply.github.com>
2017-12-27 03:22:34 -0500

    Initial Commit

---
+++ CoinAudit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoinAudit
+//
+//  Created by Ty Schenk on 12/27/17.
+//  Copyright © 2017 Ty Schenk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
